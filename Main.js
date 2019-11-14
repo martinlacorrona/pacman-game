690c7d0b3fd2470e5ad1ce25fd4a4081658b9d14
Martin Fernandez Prieto <tatin1998@gmail.com>
2019-11-14 12:06:24 +0100

    0.2.7.1: ahora solo hace 30 fotogramas, aunque el resto si que lo evalua de otra manera

--- Main.js
+++ Main.js
@@ -12,6 +12,8 @@ var layer;
 var gameLayer;
 var menuLayer;
 
+var fotograma = 0;
+
 // Inicio capas y bucle del juego
 function iniciarJuego() {
     menuLayer = new MenuLayer();
@@ -22,12 +24,17 @@ function iniciarJuego() {
 
 
 function loop(){
+    //Para que solo haga 30 fotogramas, aunque el resto se actualice cada menos
+    if((fotograma + 0.0) % factorFotogramas == 0) {
+        this.layer.dibujar();
+    }
+    fotograma++;
+
     layer.actualizar();
     if (entrada == entradas.pulsaciones) {
         layer.calcularPulsaciones(pulsaciones);
     }
     layer.procesarControles();
-    layer.dibujar();
 
     actualizarPulsaciones();
 }
